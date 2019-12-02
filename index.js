bf7988b08bd9a5b0d62e8c030beecdcd7f531434
muli c <muli3203@gmail.com>
2019-12-02 14:23:15 +0300

    declared a function

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function myFunction() {
-  var yearOb, monthOb, dateOb;
+  var yearbirth, monthbirth, datebirth;
   var male, female;
-  yearOb = parseInt(document.getElementById("year").value);
-  monthOb = parseInt(document.getElementById("month").value);
-  dateOb = parseInt(document.getElementById("date").value);
+  yearbirth = parseInt(document.getElementById("year").value);
+  monthbirth = parseInt(document.getElementById("month").value);
+  datebirth = parseInt(document.getElementById("date").value);
   male = document.getElementById("male").checked;
   female = document.getElementById("female").checked;
 
@@ -39,16 +39,14 @@ function myFunction() {
   var dayIndex;
   var dayChosen;
 
-  if (yearOb != "" && yearOb != null && yearOb > 0 && yearOb < 2020) {
-    if (monthOb != "" && monthOb != null && monthOb > 0 && monthOb < 13) {
-      if (dateOb != "" && dateOb != null && dateOb > 0 && dateOb < 32) {
+  if (yearbirth != "" && yearbirth != null && yearbirth > 0 && yearbirth < 2020) {
+    if (monthbirth != "" && monthbirth != null && monthbirth > 0 && monthbirth < 13) {
+      if (datebirth != "" && datebirth != null && datebirth > 0 && datebirth < 32) {
 
-        if (parseInt(yearOb) % 4 == 0 && parseInt(yearOb) % 100 == 0 && parseInt(yearOb) % 400 == 0) {
-          if (parseInt(monthOb) == 2 && parseInt(dateOb) > 29) {
+        if (parseInt(yearbirth) % 4 == 0 && parseInt(yearbirth) % 100 == 0 && parseInt(yearbirth) % 400 == 0) {
+          if (parseInt(monthbirth) == 2 && parseInt(datebirth) > 29) {
 
             alert("You have entered a wrong date");
-            document.getElementById("date").style.border = "2px solid red";
-            document.myForm.date.focus();
             return false;
           } else {
             getName();
@@ -83,20 +81,16 @@ function myFunction() {
 
   function getName() {
     function calculateDayIndex() {
-      dayChosen = new Date(yearOb, monthOb - 1, dateOb);
+      dayChosen = new Date(yearbirth, monthbirth - 1, datebirth);
       dayIndex = dayChosen.getDay();
       return dayIndex;
     }
 
     if (male) {
-      alert("Your were born on a " + weekDays[calculateDayIndex()]);
-      document.getElementById("check").style.color = "red";
-      document.getElementById("display").innerHTML = "Your male Akan Name is " + maleNames[calculateDayIndex()];
+      alert("Your were born on a " + weekDays[calculateDayIndex()]+"  "+"Your male Akan Name is "+ maleNames[calculateDayIndex() ]);
 
     } else {
-      alert("Your were born on a " + weekDays[calculateDayIndex()]);
-      document.getElementById("check").style.color = "red";
-      document.getElementById("display").innerHTML = "Your female Akan Name is " + femaleNames[calculateDayIndex()];
+      alert("Your were born on a " + weekDays[calculateDayIndex()] + " " +"Your female Akan Name is " + femaleNames[calculateDayIndex()]);
 
     }
 
